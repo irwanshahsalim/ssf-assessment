0956e9c92754b99abb96e26cda3d2babbabfaccb
Shiyi <yeoshiyi22@gmail.com>
2023-03-03 12:29:09 +0800

    completed task 2

--- src/main/java/ibfbatch2ssf/ssfassessment/controller/PurchaseOrderController.java
+++ src/main/java/ibfbatch2ssf/ssfassessment/controller/PurchaseOrderController.java
@@ -7,8 +7,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import ibfbatch2ssf.ssfassessment.model.*;
-import ibfbatch2ssf.ssfassessment.service.*;
+import ibfbatch2ssf.ssfassessment.model.Cart;
+import ibfbatch2ssf.ssfassessment.model.Delivery;
+import ibfbatch2ssf.ssfassessment.model.Item;
+import ibfbatch2ssf.ssfassessment.service.PurchaseService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
@@ -35,7 +37,7 @@ public class PurchaseOrderController {
         return "view1"; 
     }
 
-    // post add item 
+    // post add button 
     @PostMapping("/")
     public String postAdd(@Valid Item item, BindingResult result, Model model, HttpSession session) {
 
@@ -48,13 +50,15 @@ public class PurchaseOrderController {
             model.addAttribute("cart", cart); 
             return "view1"; 
         }
-
         // perform aggregation 
         cart = purchaseSvc.aggregate(cart, item); 
         session.setAttribute("cart", cart);
+        session.setAttribute("item", item);
 
         model.addAttribute("item", item); 
-        model.addAttribute("cart", cart);
+        model.addAttribute("cart", cart);        
+
+
 
         return "view1"; 
     }
@@ -63,8 +67,45 @@ public class PurchaseOrderController {
     @GetMapping("/shippingaddress")
     public String getAddress(Model model, HttpSession session) {
 
+        Cart cart = (Cart) session.getAttribute("cart"); 
+        System.out.println(cart); 
+
+        // cannot navigate to view 2 without a valid cart
+        if (cart.getContents().isEmpty()) {
+            model.addAttribute("item", new Item());
+            model.addAttribute("cart", cart);            
+            return "view1"; 
+        }
+
+        // task 2 
+        model.addAttribute("delivery", new Delivery());
+
+        Delivery delivery = (Delivery) session.getAttribute("delivery"); 
+        if (null == delivery) {
+            delivery = new Delivery(); 
+            session.setAttribute("delivery", delivery);
+        }
+
+        model.addAttribute("delivery", delivery);      
 
         return "view2"; 
+    }
+
+    @PostMapping("/quotation")
+    public String postAddress(@Valid Delivery delivery, BindingResult result, HttpSession session, Model model) { 
+
+        // check for delivery details errors 
+        if (result.hasErrors()) {
+            model.addAttribute("delivery", delivery); 
+            return "view2"; 
+        }
+
+        // if no error, continue 
+
+
+        // make HTTP call to get quotation (svc)
+
+        return "view3"; 
     }
     
 }
---
+++ src/main/java/ibfbatch2ssf/ssfassessment/model/Delivery.java
@@ -0,0 +1,26 @@
+package ibfbatch2ssf.ssfassessment.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Delivery {
+
+    // attributes
+    @NotNull(message="Please fill in your name")
+    @NotEmpty(message="Please fill in your name")
+    @Size(min=2, message="Name must be at least 2 characters long")
+    private String name; 
+
+    @NotNull(message="Please fill in your address")
+    @NotEmpty(message="Please fill in your address")
+    private String address;
+
+    // getter setter
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;} 
+    
+    
+}
